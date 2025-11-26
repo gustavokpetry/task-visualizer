1dcd4b4bd529344c66f76198544a0738f98421cd
Gustavo K Petry <gustavo.petry@clinicorp.com>
2025-11-26 15:48:17 -0300

    estrutura mínima funcionando

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Task Visualizer carregado");
+
+// só pra garantir que o JS está pegando o form
+const form = document.querySelector("#task-form");
+console.log("Form encontrado:", form);
